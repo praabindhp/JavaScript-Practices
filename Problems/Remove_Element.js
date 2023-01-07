2502cb8d53533d9c30fc7d9c3f0faa9ba6b8b747
praabindhp <praabindhp@gmail.com>
2023-01-07 23:23:40 +0530

    JS Practices

--- Problems/Remove_Element.js
+++ Problems/Remove_Element.js
@@ -2,12 +2,13 @@ let finalArray = [];
 let nums = [0, 1, 2, 2, 3, 0, 4, 2];
 let val = 3;
 var removeElement = function(nums, val) {
+    let j = 0;
     for (let i = 0; i < nums.length; i++) {
-        if ((nums[i]) != val) {
-            finalArray.push(nums[i]);
+        if (nums[i] !== val) {
+            nums[j++] = nums[i];
         }
     }
-    console.log(finalArray);
+    console.log(j);
 }
 
 removeElement(nums, val);
