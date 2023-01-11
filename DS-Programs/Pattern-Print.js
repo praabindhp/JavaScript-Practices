9e32cf3aba311f40fcf78e26f87bd1d754b158be
praabindh <praabindh@codingmart.com>
2023-01-11 11:14:10 +0530

    Triangle Pattern

--- DS-Programs/Pattern-Print.js
+++ DS-Programs/Pattern-Print.js
@@ -1,14 +1,24 @@
-let n = 4;
-
-let variable = 1;
-
-let pattern = "";
-
-for (let n = 1; n <= n; n++) {
-    for (let num = 1; num <= n; num++) {
-        pattern += variable + "*";
-        variable++;
+const readline = require('readline-sync');
+let n = readline.question("Enter The Number Of Rows : ");
+let str = '';
+let str1 = '';
+let sum = 1;
+let rows = n;
+let sumBelow = 1;
+for (let i = 0; i < n; i++) {
+    str = '';
+    str1 = '';
+    for (let j = 0; j < i; j++) {
+        str += '    ';
     }
-    pattern += "\n";
+    let k = (rows) * (rows);
+    for (let j = 0; j < n - i; j++) {
+        str += sum + ' * ';
+        str1 += k + sum + ' * ';
+        sum++;
+        if (j == rows - 1) str += str1 + "\n";
+
+    }
+    rows--;
+    console.log(str);
 }
-console.log(pattern);
