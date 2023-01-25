98283328800bcfc96cfb8a81ffc78c6292feb870
praabindh <praabindh@codingmart.com>
2023-01-25 16:29:52 +0530

    JavaScript Workings

---
+++ Problems/House_Robber-II.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    if (nums.length === 1) {
+        return nums[0]
+    }
+
+    function robSubset(subset = []) {
+        let rob1 = 0
+        let rob2 = 0
+        
+        for (let i = 0; i < subset.length; i++) {
+            let temp = Math.max(subset[i] + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        }
+
+        return rob2
+    }
+
+    return Math.max(robSubset(nums.slice(1)), robSubset(nums.slice(0, -1)))
+};
