761d6719931b4f9178fa44429667d3fcbf382aa7
praabindh <praabindh@codingmart.com>
2023-02-06 16:53:10 +0530

    Python3 Parser

--- Problems/Flood-Fill.js
+++ Problems/Flood-Fill.js
@@ -5,11 +5,13 @@
  * @param {number} color
  * @return {number[][]}
  */
+
 var floodFill = function(image, sr, sc, color) {
     if(image[sr][sc] == color) return image;
     fill(image, sr, sc, color, image[sr][sc]);
     return image;
 }
+
 var fill = function(image, sr, sc, color, cur) {
     if(sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length) return;
     if(cur != image[sr][sc]) return;
