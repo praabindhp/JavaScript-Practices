a1a76ee47c63022cd06af71aed5cb60d19e68b3d
praabindh <praabindh@codingmart.com>
2023-02-14 10:22:23 +0530

    Worked On Praabindh's Farm Project

---
+++ Problems/Add-Binary.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+
+var addBinary = function(a, b) {
+
+    let sum = BigInt(`0b${a}`) + BigInt(`0b${b}`);
+    return sum.toString(2);
+    
+};
