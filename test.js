452ce3dc2940051857d4eb8e335a63d573e3fdc0
praabindh <praabindh@codingmart.com>
2023-06-13 13:26:39 +0530

    Working On Advanced JavaScript

--- test.js
+++ test.js
@@ -1,26 +1,58 @@
-describe("pow", function() {
+let a = 10;
+let b = 20;
 
-    describe("raises x to power 3", function() {
+console.log("The Initial Value For A : " + a);
+console.log("The Initial Value For B : " + b + "\n");
 
-        function makeTest(x) {
-            let expected = x * x * x;
-            it(`${x} in the power 3 is ${expected}`, function() {
-                assert.equal(pow(x, 3), expected);
-            });
-        }
+let temp = a;
+a = b;
+b = temp;
 
-        for (let x = 1; x <= 5; x++) {
-            makeTest(x);
-        }
+console.log("The Swapped Value For A : " + a);
+console.log("The Swapped Value For B : " + b + "\n");
 
-    });
+for(let i = 1; i <= 10; i++){
+    console.log(i)
+}
 
-    it("if n is negative, the result is NaN", function() {
-        assert.isNaN(pow(2, -1));
-    });
+let x = 10
+let y = 0
 
-    it("if n is not integer, the result is NaN", function() {
-        assert.isNaN(pow(2, 1.5));
-    });
 
-});
+while(y < x){
+    y += 1
+    console.log("\nWorking With Value Y : " + y )
+}
+
+function fullName(firstName, lastName){
+    return firstName + " " + lastName
+}
+
+console.log("\n" + fullName("Praabindh", "Pradeep"))
+
+// describe("pow", function() {
+
+//     describe("raises x to power 3", function() {
+
+//         function makeTest(x) {
+//             let expected = x * x * x;
+//             it(`${x} in the power 3 is ${expected}`, function() {
+//                 assert.equal(pow(x, 3), expected);
+//             });
+//         }
+
+//         for (let x = 1; x <= 5; x++) {
+//             makeTest(x);
+//         }
+
+//     });
+
+//     it("if n is negative, the result is NaN", function() {
+//         assert.isNaN(pow(2, -1));
+//     });
+
+//     it("if n is not integer, the result is NaN", function() {
+//         assert.isNaN(pow(2, 1.5));
+//     });
+
+// });
